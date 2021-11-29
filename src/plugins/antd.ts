549311b772699a4d3c8d5ca0ccda8836d0461996
v_zihzhong <v_zihzhong@tencent.com>
2021-11-29 09:03:12 +0800

    feat: 注册发送邮箱验证码

--- src/plugins/antd.ts
+++ src/plugins/antd.ts
@@ -1,7 +1,7 @@
 import { App } from 'vue';
-import { message, Button, Layout, Table, Form, Tabs, Input, Menu } from 'ant-design-vue';
+import { message, Button, Layout, Table, Form, Tabs, Input, Menu, Row, Col } from 'ant-design-vue';
 
-const components = [Button, Layout, Table, Form, Tabs, Input, Menu];
+const components = [Button, Layout, Table, Form, Tabs, Input, Menu, Row, Col];
 
 export default function initAntd(app: App): void {
   // 按需注册 antd 的组件
